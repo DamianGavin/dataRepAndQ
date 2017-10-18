9b5fdfc6c4d6bf75769269922ffac40b6c569617
DAMIEN GAVIN - Student <10007969@gmit.ie>
2017-10-18 10:03:50 +0100

    addedCookieCounter

--- counter.go
+++ counter.go
@@ -1,15 +1,41 @@
+//Damian Gavin 18/10/17
+//using cookies as a counter
+//track how many times a user has been to your website domain
+
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hello World")
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
 }
 
-func main() {
-    http.HandleFunc("/", helloWorld)
-    http.ListenAndServe(":8080", nil)
+func foo(res http.ResponseWriter, req *http.Request) {
+
+	cookie, err := req.Cookie("my-cookie")
+
+	if err == http.ErrNoCookie {
+		cookie = &http.Cookie{
+			Name:  "my-cookie",
+			Value: "0",
+		}
+	}
+
+	count, err := strconv.Atoi(cookie.Value)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	count++
+	cookie.Value = strconv.Itoa(count)
+
+	http.SetCookie(res, cookie)
+
+	io.WriteString(res, cookie.Value)
 }
